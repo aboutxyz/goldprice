0a94ed76939a163c33fc2476e825a5879adfd75e
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-02-09 16:56:52 +0800

    fff

--- webgold.py
+++ webgold.py
@@ -1,12 +1,14 @@
 #coding:utf-8
 
 from flask import Flask
+import mako
 
 app = Flask(__name__)
+mako = MakoTemplates(app)
 
 @app.route('/')
 def hello():
-    return "hello"
+    return render_template('index.html', name = 'mako')
     
 if __name__ == '__main__':
     app.run()
