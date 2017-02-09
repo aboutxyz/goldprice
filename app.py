9d2f4188872d758308b3d3f73e1aa0a0c8a00e03
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-02-09 21:45:29 +0800

    update

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 #coding:utf-8
 
 from flask import Flask
-import mako
+from flask_mako import MakoTemplates, render_template
 
 app = Flask(__name__)
 mako = MakoTemplates(app)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html', name = 'mako')
     
 if __name__ == '__main__':
-    app.run()
+    app.run(port = 8989)
