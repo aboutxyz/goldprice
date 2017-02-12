448e9dfb234056a845869b90652a876c19846fb6
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-02-12 16:05:23 +0800

    jiji

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 #coding:utf-8
 
-from flask import Flask
-from flask_mako import MakoTemplates, render_template
+from flask import Flask, render_template, url_for
+import json
+# from flask_mako import MakoTemplates, render_template
 
 app = Flask(__name__)
-mako = MakoTemplates(app)
+# mako = MakoTemplates(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', name = 'mako')
-    
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run(port = 8989)
+    app.run(port = 9999, debug=True)
