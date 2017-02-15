9059185c453b154fc4a87270764af0f7e8111cce
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-02-15 15:40:30 +0800

    need to be done

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 # coding:utf-8
-from mako.template import Template
+import json
 
-mytemplate = Template("hello world!")
-print(mytemplate.render())
+with open("goldhistory.json", "r") as f:
+    for i in json.loads(f.read()):
+        if i["fld_name"] == u"足金零售价":
+            print i["fld_newprice"]
