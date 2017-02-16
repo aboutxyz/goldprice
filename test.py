39f1a627d7f19ade2836e5b8f9d00cfc7567d326
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-02-16 16:14:27 +0800

    success version 1

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 # coding:utf-8
 import json
 
+pricelist = []
 with open("goldhistory.json", "r") as f:
     for i in json.loads(f.read()):
         if i["fld_name"] == u"足金零售价":
-            print i["fld_newprice"]
+            print i["fld_newprice"],i["fld_secudate"]
+            pricelist.append([i["fld_secudate"],float(i["fld_newprice"])])
+
+encode_json = json.dumps(pricelist)            
+with open("golddata.json", "a") as boom:
+    boom.write(encode_json)
